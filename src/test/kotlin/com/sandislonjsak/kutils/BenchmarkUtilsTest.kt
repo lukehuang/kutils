8be52dc6b8f33f81e86e81c2ee2c005152c9c939
Sandi Slonjšak <sandi.slonjsak@gmail.com>
2018-02-03 10:05:35 +0100

    refactor(BenchmarkUtilsTest): add more reliable test

--- src/test/kotlin/com/sandislonjsak/kutils/BenchmarkUtilsTest.kt
+++ src/test/kotlin/com/sandislonjsak/kutils/BenchmarkUtilsTest.kt
@@ -4,27 +4,13 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class BenchmarkUtilsTest {
-  //TODO: This is a bad test and needs to be refactored
   @Test
-  fun `test measureExecTimeMillis pass function as an arg`() {
-    fun func() = (0..100000).forEach {
-      if (it % 99999 == 0) println("test")
-    }
+  fun `test measureExecTimeMillis`() {
+    fun func() = Thread.sleep(2)
 
-    val result = BenchmarkUtils.measureExecTimeMillis(::func)
-
-    assertEquals(true, result > 1L)
-  }
-
-  //TODO: This is a bad test and needs to be refactored
-  @Test
-  fun `test measureExecTimeMillis pass lambda expression`() {
-    val result = BenchmarkUtils.measureExecTimeMillis {
-      (0..100000).forEach {
-        if (it % 99999 == 0) println("test")
-      }
-    }
-
-    assertEquals(true, result > 1L)
+    assertEquals(true, BenchmarkUtils.measureExecTimeMillis(::func) > 1L)
+    assertEquals(true, BenchmarkUtils.measureExecTimeMillis {
+      Thread.sleep(2)
+    } > 1L)
   }
 }
