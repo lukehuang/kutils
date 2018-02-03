b49976a95785c3c91dfdb7cc2af1015952643f87
Sandi Slonjšak <sandi.slonjsak@gmail.com>
2018-02-03 10:04:15 +0100

    feat(DateUtils): add more functions for adding and subtracting days and seconds
    - implement addDays
    - implement addSeconds
    - implement subtractDays
    - implement subtractSeconds

--- src/main/kotlin/com/sandislonjsak/kutils/DateUtils.kt
+++ src/main/kotlin/com/sandislonjsak/kutils/DateUtils.kt
@@ -7,6 +7,20 @@ import java.util.*
  */
 object DateUtils {
   /**
+   * Adds days to target Date.
+   * If days are negative returns date without modifying it.
+   *
+   * @param date Date to which days need to be added.
+   * @param days Amount of days to be added to date.
+   * @return Date plus days
+   */
+  fun addDays(date: Date, days: Int): Date {
+    if (days <= 0) return date
+
+    return Date(date.time + 1000 * 60 * 60 * 24 * days)
+  }
+
+  /**
    * Adds minutes to target Date.
    * If minutes are negative returns date without modifying it.
    *
@@ -21,6 +35,34 @@ object DateUtils {
   }
 
   /**
+   * Adds seconds to target Date.
+   * If seconds are negative returns date without modifying it.
+   *
+   * @param date Date to which seconds need to be added.
+   * @param seconds Amount of seconds to be added to date.
+   * @return Date plus seconds
+   */
+  fun addSeconds(date: Date, seconds: Int): Date {
+    if (seconds <= 0) return date
+
+    return Date(date.time + 1000 * seconds)
+  }
+
+  /**
+   * Subtract days from target Date.
+   * If days are negative returns date without modifying it.
+   *
+   * @param date Date from which days need to be subtracted.
+   * @param days Amount of days to be subtracted from date.
+   * @return Date minus days
+   */
+  fun subtractDays(date: Date, days: Int): Date {
+    if (days <= 0) return date
+
+    return Date(date.time - 1000 * 60 * 60 * 24 * days)
+  }
+
+  /**
    * Subtracts minutes from target Date.
    * If minutes are negative returns date without modifying it.
    *
@@ -32,5 +74,19 @@ object DateUtils {
     if (minutes <= 0) return date
 
     return Date(date.time - 1000 * 60 * minutes)
+  }
+
+  /**
+   * Subtracts seconds from target Date.
+   * If seconds are negative returns date without modifying it.
+   *
+   * @param date Date from which seconds need to be subtracted.
+   * @param seconds Amount of seconds to be subtracted from date.
+   * @return Date minus seconds
+   */
+  fun subtractSeconds(date: Date, seconds: Int): Date {
+    if (seconds <= 0) return date
+
+    return Date(date.time - 1000 * seconds)
   }
 }
