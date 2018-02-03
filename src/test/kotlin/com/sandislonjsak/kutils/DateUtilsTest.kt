24689bf7c25a84cfc38eb8bf562bf0ff432c0ae4
Sandi Slonjšak <sandi.slonjsak@gmail.com>
2018-02-03 10:07:15 +0100

    test(DateUtilsTest): test adding and subtracting days and seconds
    - test addDays function
    - test addSeconds function
    - test subtractDays function
    - test subtractSeconds function
    - add more test cases to each test to be more accurate and assured that implementations work as expected

--- src/test/kotlin/com/sandislonjsak/kutils/DateUtilsTest.kt
+++ src/test/kotlin/com/sandislonjsak/kutils/DateUtilsTest.kt
@@ -6,30 +6,56 @@ import java.util.*
 
 class DateUtilsTest {
   @Test
-  fun `test addMinutes add ten minutes`() {
+  fun `test addDays`(){
     val date = Date()
 
-    assertEquals(Date(date.time + 600000), DateUtils.addMinutes(date, 10))
+    assertEquals(Date(date.time + 86400000), DateUtils.addDays(date, 1))
+    assertEquals(Date(date.time + 172800000), DateUtils.addDays(date, 2))
+    assertEquals(date, DateUtils.addDays(date, -10))
   }
 
   @Test
-  fun `test addMinutes add negative minutes`() {
+  fun `test addMinutes`() {
     val date = Date()
 
+    assertEquals(Date(date.time + 600000), DateUtils.addMinutes(date, 10))
+    assertEquals(Date(date.time + 6000000), DateUtils.addMinutes(date, 100))
     assertEquals(date, DateUtils.addMinutes(date, -10))
   }
 
   @Test
-  fun `test subtractMinutes subtract ten minutes`() {
+  fun `test addSeconds`() {
     val date = Date()
 
-    assertEquals(Date(date.time - 600000), DateUtils.subtractMinutes(date, 10))
+    assertEquals(Date(date.time + 10000), DateUtils.addSeconds(date, 10))
+    assertEquals(Date(date.time + 100000), DateUtils.addSeconds(date, 100))
+    assertEquals(date, DateUtils.addSeconds(date, -10))
   }
 
   @Test
-  fun `test subtractMinutes subtract negative minutes`() {
+  fun `test subtractDays`(){
     val date = Date()
 
+    assertEquals(Date(date.time - 86400000), DateUtils.subtractDays(date, 1))
+    assertEquals(Date(date.time - 172800000), DateUtils.subtractDays(date, 2))
+    assertEquals(date, DateUtils.subtractDays(date, -10))
+  }
+
+  @Test
+  fun `test subtractMinutes`() {
+    val date = Date()
+
+    assertEquals(Date(date.time - 600000), DateUtils.subtractMinutes(date, 10))
+    assertEquals(Date(date.time - 60000), DateUtils.subtractMinutes(date, 1))
     assertEquals(date, DateUtils.subtractMinutes(date, -10))
+  }
+
+  @Test
+  fun `test subtractSeconds`() {
+    val date = Date()
+
+    assertEquals(Date(date.time - 10000), DateUtils.subtractSeconds(date, 10))
+    assertEquals(Date(date.time - 100000), DateUtils.subtractSeconds(date, 100))
+    assertEquals(date, DateUtils.addSeconds(date, -10))
   }
 }
